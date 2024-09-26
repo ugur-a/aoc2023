6ce6698f43d72d50fccc427ec42095b8e65912f3
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-09-26 14:35:45 +0200

    feat(d01): p1 done!

--- d01/src/lib.rs
+++ d01/src/lib.rs
@@ -1,5 +1,20 @@
-pub fn p1(_file: &str) -> anyhow::Result<u32> {
-    todo!()
+use anyhow::Context;
+
+pub fn p1(file: &str) -> anyhow::Result<u32> {
+    let mut res = 0;
+    for line in file.lines() {
+        let first = line
+            .chars()
+            .find_map(|c| c.to_digit(10))
+            .context("no digits")?;
+        let second = line
+            .chars()
+            .rev()
+            .find_map(|c| c.to_digit(10))
+            .context("only one digit")?;
+        res += 10 * first + second;
+    }
+    Ok(res)
 }
 
 #[cfg(test)]
@@ -10,7 +25,7 @@ mod test {
     const REAL: &str = include_str!("../inputs/real.txt");
 
     #[test_case(EXAMPLE => 142)]
-    #[test_case[REAL => ignore 0]]
+    #[test_case[REAL => 55712]]
     fn test_p1(inp: &str) -> u32 {
         p1(inp).unwrap()
     }
