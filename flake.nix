a51da812b225b00db0a42823b47742f2b54832e0
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-09-26 14:23:49 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+          ];
+        };
+      }
+    );
+}
