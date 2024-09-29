cc5d74ff9df0483274487f897713bbaf7931962e
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-09-29 13:17:40 +0200

    feat(d03): p1 done!

--- d03/src/p1.rs
+++ d03/src/p1.rs
@@ -1,5 +1,32 @@
-pub fn p1(_file: &str) -> anyhow::Result<u32> {
-    todo!()
+use core::str::FromStr;
+
+use crate::schematic::{Number, Schematic, Symbol};
+
+impl Schematic {
+    fn part_numbers(&self) -> impl Iterator<Item = u32> + '_ {
+        self.numbers
+            .iter()
+            .filter(
+                |&&Number {
+                     start_pos: (start_x, start_y),
+                     len,
+                     ..
+                 }| {
+                    (start_x..start_x + len as usize)
+                        .map(|x| Symbol { pos: (x, start_y) })
+                        .any(|s| self.symbols.contains(&s))
+                },
+            )
+            .map(|n| n.value)
+    }
+}
+
+pub fn p1(file: &str) -> anyhow::Result<u32> {
+    let s = Schematic::from_str(file)?;
+
+    let res = s.part_numbers().sum();
+
+    Ok(res)
 }
 
 #[cfg(test)]
@@ -10,7 +37,7 @@ mod test {
     const REAL: &str = include_str!("../inputs/real.txt");
 
     #[test_case(EXAMPLE => 4361)]
-    #[test_case[REAL => ignore 0]]
+    #[test_case[REAL => 536_576]]
     fn test_p1(inp: &str) -> u32 {
         p1(inp).unwrap()
     }
