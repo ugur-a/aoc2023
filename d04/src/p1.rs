ab76a4b246a5ce30c5ba95abc02f90663c3b252f
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-09-29 22:03:33 +0200

    feat(d04): p1 done!

--- d04/src/p1.rs
+++ d04/src/p1.rs
@@ -1,5 +1,27 @@
-pub fn p1(_file: &str) -> anyhow::Result<u32> {
-    todo!()
+use core::str::FromStr;
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+use crate::card::Card;
+
+impl Card {
+    fn worth(&self) -> u32 {
+        let nwinning =
+            HashSet::intersection(&self.winning_numbers, &self.your_numbers).count() as u32;
+        match nwinning {
+            0 => 0,
+            _ => 2u32.pow(nwinning - 1),
+        }
+    }
+}
+
+pub fn p1(file: &str) -> anyhow::Result<u32> {
+    let cards: Vec<_> = file.lines().map(Card::from_str).try_collect()?;
+
+    let res = cards.iter().map(Card::worth).sum();
+
+    Ok(res)
 }
 
 #[cfg(test)]
@@ -10,7 +32,7 @@ mod test {
     const REAL: &str = include_str!("../inputs/real.txt");
 
     #[test_case(EXAMPLE => 13)]
-    #[test_case[REAL => ignore]]
+    #[test_case[REAL => 20829]]
     fn test_p1(inp: &str) -> u32 {
         p1(inp).unwrap()
     }
