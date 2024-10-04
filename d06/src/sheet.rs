832733b4fb73843d4f76774b4d1ff3487b0a6bde
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-04 12:52:03 +0200

    perf(d06): don't check _all_ the strategies, find the 1st one that works
    .. and calculate the answer from there

--- d06/src/sheet.rs
+++ d06/src/sheet.rs
@@ -5,9 +5,18 @@ pub(crate) struct Race {
 
 impl Race {
     pub(crate) fn ways_to_win(&self) -> usize {
-        (1..self.time)
-            .map(|t| t * (self.time - t))
-            .filter(|dist| dist > &self.distance)
-            .count()
+        let mut options = (1..self.time).map(|t| t * (self.time - t));
+
+        let min_time = options.position(|dist| dist > self.distance);
+
+        if let Some(t_min) = min_time {
+            // by the distance formula, `t_max := self.time - t_min`
+            // and an off-by-one, because of course there is one
+            let t_max = self.time as usize - t_min - 1;
+            t_max - t_min
+        } else {
+            // no way to win the race
+            0
+        }
     }
 }
