3526bf25ce30d025a99e48b61b8f78cf616fd1d4
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-05 12:22:09 +0200

    chore(d08): init p2

--- d08/src/p2.rs
+++ d08/src/p2.rs
@@ -6,12 +6,10 @@ pub fn p2(_file: &str) -> anyhow::Result<u32> {
 mod test {
     use super::*;
     use test_case::test_case;
-    const EXAMPLE1: &str = include_str!("../inputs/example1.txt");
-    const EXAMPLE2: &str = include_str!("../inputs/example2.txt");
+    const EXAMPLE: &str = include_str!("../inputs/example3.txt");
     const REAL: &str = include_str!("../inputs/real.txt");
 
-    #[test_case(EXAMPLE1 => ignore)]
-    #[test_case(EXAMPLE2 => ignore)]
+    #[test_case(EXAMPLE => 6)]
     #[test_case(REAL => ignore)]
     fn test_p2(inp: &str) -> u32 {
         p2(inp).unwrap()
