30075f5c21a5607e47a22e2d41b5ed0d08bbe38b
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-05 13:33:56 +0200

    feat(08): p2 done!

--- d08/src/p2.rs
+++ d08/src/p2.rs
@@ -1,5 +1,35 @@
-pub fn p2(_file: &str) -> anyhow::Result<u32> {
-    todo!()
+use crate::map::{Map, MoveDirection};
+use num::Integer;
+
+pub fn p2(file: &str) -> anyhow::Result<u64> {
+    let is_start = |name: &&str| name.ends_with('A');
+    let is_end = |name: &str| name.ends_with('Z');
+
+    let Map { moves, nodes } = Map::try_from(file)?;
+
+    let curr_nodes: Vec<_> = nodes.0.keys().copied().filter(is_start).collect();
+
+    let times_to_reach_end = curr_nodes.into_iter().map(|mut node| {
+        let mut nmoves = 0;
+        for moove in moves.0.iter().cycle() {
+            if is_end(node) {
+                break;
+            }
+
+            let nexts = &nodes.0[node];
+            node = match moove {
+                MoveDirection::Left => nexts.left,
+                MoveDirection::Right => nexts.right,
+            };
+
+            nmoves += 1;
+        }
+        nmoves
+    });
+
+    let res = times_to_reach_end.reduce(|acc, n| acc.lcm(&n)).unwrap();
+
+    Ok(res)
 }
 
 #[cfg(test)]
@@ -10,8 +40,8 @@ mod test {
     const REAL: &str = include_str!("../inputs/real.txt");
 
     #[test_case(EXAMPLE => 6)]
-    #[test_case(REAL => ignore)]
-    fn test_p2(inp: &str) -> u32 {
+    #[test_case(REAL => 13_740_108_158_591)]
+    fn test_p2(inp: &str) -> u64 {
         p2(inp).unwrap()
     }
 }
