ce3dc1a4eb0a6a9603cf49962b44ce0541fc80f9
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-06 23:05:29 +0200

    chore(d11): init

---
+++ d11/src/p2.rs
@@ -0,0 +1,17 @@
+pub fn p2(_file: &str) -> anyhow::Result<u32> {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use test_case::test_case;
+    const EXAMPLE: &str = include_str!("../inputs/example.txt");
+    const REAL: &str = include_str!("../inputs/real.txt");
+
+    #[test_case(EXAMPLE => ignore)]
+    #[test_case(REAL => ignore)]
+    fn test_p2(inp: &str) -> u32 {
+        p2(inp).unwrap()
+    }
+}
