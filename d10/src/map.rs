a3796b7cb25e924477b1a9721903f66726dbae57
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-09 23:19:04 +0200

    feat(d10): p2 done!

--- d10/src/map.rs
+++ d10/src/map.rs
@@ -2,9 +2,31 @@ use core::str::FromStr;
 
 use anyhow::bail;
 use itertools::Itertools;
-use libaoc::points::Point2D;
+use libaoc::points::{two_d::Border2D, Point2D};
 
 type Pos = Point2D<usize>;
+
+trait NewTrait {
+    fn north(self) -> Self;
+    fn south(self) -> Self;
+    fn east(self) -> Self;
+    fn west(self) -> Self;
+}
+
+impl NewTrait for Pos {
+    fn north(self) -> Self {
+        Point2D(self.x(), self.y() - 1)
+    }
+    fn south(self) -> Self {
+        Point2D(self.x(), self.y() + 1)
+    }
+    fn east(self) -> Self {
+        Point2D(self.x() + 1, self.y())
+    }
+    fn west(self) -> Self {
+        Point2D(self.x() - 1, self.y())
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub(crate) enum PipeDirection {
@@ -16,37 +38,75 @@ pub(crate) enum PipeDirection {
     SouthEast,
 }
 
-#[derive(Debug)]
+use PipeDirection::EastWest as EW;
+use PipeDirection::NorthEast as NE;
+use PipeDirection::NorthSouth as NS;
+use PipeDirection::NorthWest as NW;
+use PipeDirection::SouthEast as SE;
+use PipeDirection::SouthWest as SW;
+
+#[derive(Debug, Clone, Copy)]
 enum Point {
     Pipe(PipeDirection),
     Ground,
-    Start,
 }
 
 impl TryFrom<char> for Point {
     type Error = anyhow::Error;
 
     fn try_from(value: char) -> Result<Self, Self::Error> {
-        use PipeDirection as PD;
         let res = match value {
-            '|' => Self::Pipe(PD::NorthSouth),
-            '-' => Self::Pipe(PD::EastWest),
-            'L' => Self::Pipe(PD::NorthEast),
-            'J' => Self::Pipe(PD::NorthWest),
-            '7' => Self::Pipe(PD::SouthWest),
-            'F' => Self::Pipe(PD::SouthEast),
+            '|' => Self::Pipe(NS),
+            '-' => Self::Pipe(EW),
+            'L' => Self::Pipe(NE),
+            'J' => Self::Pipe(NW),
+            '7' => Self::Pipe(SW),
+            'F' => Self::Pipe(SE),
             '.' => Self::Ground,
-            'S' => Self::Start,
             c => bail!("invalid direction: {c}"),
         };
         Ok(res)
     }
 }
 
+impl Point {
+    pub fn points_west(self) -> bool {
+        matches!(self, Self::Pipe(NW | SW | EW),)
+    }
+
+    pub fn points_east(self) -> bool {
+        matches!(self, Self::Pipe(NE | EW | SE))
+    }
+
+    pub fn points_south(self) -> bool {
+        matches!(self, Self::Pipe(SE | NS | SW))
+    }
+
+    pub fn points_north(self) -> bool {
+        matches!(self, Self::Pipe(NE | NS | NW))
+    }
+
+    fn turn_clockwise(self) -> Self {
+        match self {
+            Self::Ground => self,
+            Self::Pipe(pd) => {
+                let pd = match pd {
+                    NS => EW,
+                    EW => NS,
+
+                    NW => NE,
+                    NE => SE,
+                    SE => SW,
+                    SW => NW,
+                };
+                Self::Pipe(pd)
+            }
+        }
+    }
+}
+
 pub(crate) struct Map {
     inner: Vec<Vec<Point>>,
-    width: usize,
-    height: usize,
     start_pos: Pos,
 }
 
@@ -57,86 +117,82 @@ impl FromStr for Map {
         let width = s.lines().next().unwrap().len();
         let height = s.lines().count();
 
+        let start_pos = s
+            .lines()
+            .enumerate()
+            .find_map(|(y, line)| line.chars().position(|p| p == 'S').map(|x| Point2D(x, y)))
+            .unwrap();
+
+        let pipe_under_start = {
+            // NOTE: can't use `Map::get` since the instance is not constructed yet
+            let get = |Point2D(x, y)| -> Point {
+                s.lines()
+                    .nth(y)
+                    .unwrap()
+                    .chars()
+                    .nth(x)
+                    .unwrap()
+                    .try_into()
+                    .unwrap()
+            };
+
+            let p @ Point2D(x, y) = start_pos;
+
+            let pipe_to_west = x > 0 && get(p.west()).points_east();
+            let pipe_to_east = x < width - 1 && get(p.east()).points_west();
+            let pipe_to_north = y > 0 && get(p.north()).points_south();
+            let pipe_to_south = y < height - 1 && get(p.south()).points_north();
+
+            let pd = match (pipe_to_west, pipe_to_east, pipe_to_north, pipe_to_south) {
+                (true, true, false, false) => EW,
+                (true, false, true, false) => NW,
+                (true, false, false, true) => SW,
+                (false, true, true, false) => NE,
+                (false, true, false, true) => SE,
+                (false, false, true, true) => NS,
+                _ => unreachable!("there must be pipes to exactly two sides"),
+            };
+
+            Point::Pipe(pd)
+        };
+
         let mut map = Vec::with_capacity(height);
-        for line in s.lines() {
-            let line: Vec<Point> = line.chars().map(Point::try_from).try_collect()?;
+        for (y, line) in s.lines().enumerate() {
+            let line: Vec<Point> = line
+                .char_indices()
+                .map(|(x, c)| {
+                    if Point2D(x, y) == start_pos {
+                        Ok(pipe_under_start)
+                    } else {
+                        Point::try_from(c)
+                    }
+                })
+                .try_collect()?;
             map.push(line);
         }
 
-        let start_pos = map
-            .iter()
-            .enumerate()
-            .find_map(|(y, line)| {
-                line.iter()
-                    .position(|p| matches!(p, Point::Start))
-                    .map(|x| Point2D(x, y))
-            })
-            .unwrap();
-
         Ok(Self {
             inner: map,
-            width,
-            height,
             start_pos,
         })
     }
 }
 
 impl Map {
-    fn adjacent(&self, &p @ Point2D(x, y): &Pos) -> Option<[Pos; 2]> {
-        use PipeDirection as PD;
+    fn get(&self, &Point2D(x, y): &Pos) -> &Point {
+        &self.inner[y][x]
+    }
 
-        let north = |Point2D(x, y)| Point2D(x, y - 1);
-        let south = |Point2D(x, y)| Point2D(x, y + 1);
-        let east = |Point2D(x, y)| Point2D(x + 1, y);
-        let west = |Point2D(x, y)| Point2D(x - 1, y);
-
-        #[allow(clippy::match_on_vec_items)]
-        match self.inner[y][x] {
-            Point::Start => {
-                let res = core::iter::empty()
-                    // west
-                    .chain((x > 0).then(|| west(p)).filter(|p| {
-                        matches!(
-                            self.inner[p.y()][p.x()],
-                            Point::Pipe(PD::NorthEast | PD::EastWest | PD::SouthEast)
-                        )
-                    }))
-                    // east
-                    .chain((x < self.width - 1).then(|| east(p)).filter(|p| {
-                        matches!(
-                            self.inner[p.y()][p.x()],
-                            Point::Pipe(PD::NorthWest | PD::EastWest | PD::SouthWest)
-                        )
-                    }))
-                    // north
-                    .chain((y > 0).then(|| north(p)).filter(|p| {
-                        matches!(
-                            self.inner[p.y()][p.x()],
-                            Point::Pipe(PD::SouthEast | PD::NorthSouth | PD::SouthWest)
-                        )
-                    }))
-                    // south
-                    .chain((y < self.height - 1).then(|| south(p)).filter(|p| {
-                        matches!(
-                            self.inner[p.y()][p.x()],
-                            Point::Pipe(PD::NorthEast | PD::NorthSouth | PD::NorthWest)
-                        )
-                    }))
-                    .collect::<Vec<_>>()
-                    .try_into()
-                    .expect("there must be pipes to exactly two sides");
-
-                Some(res)
-            }
+    fn adjacent(&self, &p: &Pos) -> Option<[Pos; 2]> {
+        match self.get(&p) {
             Point::Pipe(pd) => {
                 let res = match pd {
-                    PD::NorthSouth => [north(p), south(p)],
-                    PD::EastWest => [east(p), west(p)],
-                    PD::NorthWest => [north(p), west(p)],
-                    PD::NorthEast => [north(p), east(p)],
-                    PD::SouthWest => [south(p), west(p)],
-                    PD::SouthEast => [south(p), east(p)],
+                    NS => [p.north(), p.south()],
+                    EW => [p.east(), p.west()],
+                    NW => [p.north(), p.west()],
+                    NE => [p.north(), p.east()],
+                    SW => [p.south(), p.west()],
+                    SE => [p.south(), p.east()],
                 };
                 Some(res)
             }
@@ -174,5 +230,128 @@ impl Map {
             (curr, next) = (next, next_next);
         }
         looop
+    }
+
+    /// counts the number of times a horizontal line from `point` crosses the loop
+    /// if the number is even, `point` is outside the loop
+    /// the opposite is not necessarily true - need to check vertically, too
+    ///  NOTE: `Pos` in the iterator is really just for debugging
+    fn is_inside_loop_right(points_right: impl Iterator<Item = (Pos, Point)>) -> bool {
+        enum EnteredFrom {
+            Up,
+            Down,
+        }
+        enum State {
+            NotOnLoop,
+            OnLoop(EnteredFrom),
+        }
+        let mut state = State::NotOnLoop;
+
+        let mut n_loop_crosses = 0;
+
+        for (pos, p) in points_right {
+            match (&state, p) {
+                (State::NotOnLoop, Point::Ground) => {
+                    // no change in status
+                }
+                (State::NotOnLoop, Point::Pipe(pd)) => {
+                    match pd {
+                        NE => {
+                            state = State::OnLoop(EnteredFrom::Up);
+                        }
+                        SE => {
+                            state = State::OnLoop(EnteredFrom::Down);
+                        }
+                        NW | SW | EW => {
+                            unreachable!("invalid loop: entered pipe {p:?} from air at {pos:?}")
+                        }
+                        NS => {
+                            // directly crossed the loop
+                            n_loop_crosses += 1;
+                        }
+                    }
+                }
+                (State::OnLoop(_), Point::Ground) => {
+                    unreachable!("invalid loop: pipe {p:?} exits to air at {pos:?}")
+                }
+                (State::OnLoop(ef), Point::Pipe(pd)) => {
+                    match (ef, pd) {
+                        (EnteredFrom::Up, NW) | (EnteredFrom::Down, SW) => {
+                            // squeezed along the loop: L-J / F-7
+                            // nothing crossed, just exit the loop
+                            state = State::NotOnLoop;
+                        }
+                        (_, EW) => {
+                            // continue along the loop
+                        }
+                        (EnteredFrom::Up, SW) | (EnteredFrom::Down, NW) => {
+                            // crossed the loop after going along it: L-7 / F-J
+                            n_loop_crosses += 1;
+                            state = State::NotOnLoop;
+                        }
+                        (_, NE | NS | SE) => {
+                            unreachable!("invalid loop: pipe {p:?} ended unexpectedly at {pos:?}")
+                        }
+                    }
+                }
+            }
+        }
+
+        n_loop_crosses % 2 == 1
+    }
+
+    pub fn is_inside_loop(
+        &self,
+        looop: &[Pos],
+        border: &Border2D<usize>,
+        point @ Point2D(x, y): Pos,
+    ) -> bool {
+        if looop.contains(&point) {
+            return false;
+        }
+
+        ({
+            // east
+            Self::is_inside_loop_right(
+                (x + 1..=border.right)
+                    .map(|x| Point2D(x, y))
+                    .filter(|p| looop.contains(p))
+                    .map(|p| (p, *self.get(&p))),
+            )
+        }) && {
+            // north
+            Self::is_inside_loop_right(
+                (border.top..y)
+                    .rev()
+                    .map(|y| Point2D(x, y))
+                    .filter(|p| looop.contains(p))
+                    .map(|p| (p, self.get(&p).turn_clockwise())),
+            )
+        } && {
+            // west
+            Self::is_inside_loop_right(
+                (border.left..x)
+                    .rev()
+                    .map(|x| Point2D(x, y))
+                    .filter(|p| looop.contains(p))
+                    .map(|p| (p, self.get(&p).turn_clockwise().turn_clockwise())),
+            )
+        } && {
+            // south
+            Self::is_inside_loop_right(
+                (y..=border.down)
+                    .map(|y| Point2D(x, y))
+                    .filter(|p| looop.contains(p))
+                    .map(|p| {
+                        (
+                            p,
+                            self.get(&p)
+                                .turn_clockwise()
+                                .turn_clockwise()
+                                .turn_clockwise(),
+                        )
+                    }),
+            )
+        }
     }
 }
--- d10/src/p2.rs
+++ d10/src/p2.rs
@@ -1,19 +1,48 @@
-pub fn p2(_file: &str) -> anyhow::Result<u32> {
-    todo!()
+use core::str::FromStr;
+
+use crate::map::Map;
+use libaoc::points::{
+    two_d::{min_enclosing_rectangle, Border2D},
+    Point2D,
+};
+
+pub fn p2(file: &str) -> anyhow::Result<usize> {
+    let map = Map::from_str(file)?;
+    // eprintln!("{map}");
+
+    let looop = map.find_loop();
+
+    let border @ Border2D {
+        left,
+        right,
+        down,
+        top,
+    } = min_enclosing_rectangle(looop.iter(), looop.iter());
+
+    let res = (top..=down)
+        .flat_map(|y| (left..=right).map(move |x| Point2D(x, y)))
+        .filter(|&p| map.is_inside_loop(&looop, &border, p))
+        .count();
+
+    Ok(res)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     use test_case::test_case;
-    const EXAMPLE1: &str = include_str!("../inputs/example1.txt");
-    const EXAMPLE2: &str = include_str!("../inputs/example2.txt");
+    const EXAMPLE3: &str = include_str!("../inputs/example3.txt");
+    const EXAMPLE4: &str = include_str!("../inputs/example4.txt");
+    const EXAMPLE5: &str = include_str!("../inputs/example5.txt");
+    const EXAMPLE6: &str = include_str!("../inputs/example6.txt");
     const REAL: &str = include_str!("../inputs/real.txt");
 
-    #[test_case(EXAMPLE1 => ignore)]
-    #[test_case(EXAMPLE2 => ignore)]
-    #[test_case(REAL => ignore)]
-    fn test_p2(inp: &str) -> u32 {
+    #[test_case(EXAMPLE3 => 4)]
+    #[test_case(EXAMPLE4 => 4)]
+    #[test_case(EXAMPLE5 => 8)]
+    #[test_case(EXAMPLE6 => 10)]
+    #[test_case(REAL => 449)]
+    fn test_p2(inp: &str) -> usize {
         p2(inp).unwrap()
     }
 }
