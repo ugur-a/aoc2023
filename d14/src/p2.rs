ceb8dff668b51f4f088402104b1d0dcc2de39aad
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-21 13:36:23 +0200

    feat(d14): p2 done!

--- d14/src/p2.rs
+++ d14/src/p2.rs
@@ -1,7 +1,51 @@
 use core::str::FromStr;
+use std::collections::HashMap;
 
-pub fn p2(_file: &str) -> anyhow::Result<usize> {
-    todo!()
+use crate::platform::Platform;
+
+const N_CYCLES: usize = 1_000_000_000;
+
+impl Platform {
+    fn cycle(&mut self) {
+        self.tilt_north();
+        self.tilt_west();
+        self.tilt_south();
+        self.tilt_east();
+    }
+}
+
+pub fn p2(file: &str) -> anyhow::Result<usize> {
+    let mut p = Platform::from_str(file)?;
+
+    let mut states = HashMap::new();
+
+    let mut yet_to_run = None;
+    for i in 0..N_CYCLES {
+        p.cycle();
+
+        if let Some(j) = states.insert(p.clone(), i) {
+            eprintln!("cycle found: {j}->{i}");
+            // let s(i) := state after round i
+            // then:
+            // we're in this condition
+            // => s(i) = s(j)
+            // => after round j, there are repeated cycles of length (i-j)
+            // => for all k: s(j + (i-j)k) = s(i)
+            // => s(n) = s((n-j)%(i-j))
+            // => run for (N_STATES-j)%(i-j) _more_ cycles
+            // => will achieve a state equivalent to s(N_STATES)
+            yet_to_run = Some((N_CYCLES - 1 - j) % (i - j));
+            break;
+        }
+    }
+
+    if let Some(n) = yet_to_run {
+        for _ in 0..n {
+            p.cycle();
+        }
+    }
+
+    Ok(p.north_load())
 }
 
 #[cfg(test)]
@@ -11,8 +55,8 @@ mod test {
     const EXAMPLE: &str = include_str!("../inputs/example.txt");
     const REAL: &str = include_str!("../inputs/real.txt");
 
-    #[test_case(EXAMPLE => ignore)]
-    #[test_case(REAL => ignore)]
+    #[test_case(EXAMPLE => 64)]
+    #[test_case(REAL => 103_445)]
     fn test_p2(inp: &str) -> usize {
         p2(inp).unwrap()
     }
--- d14/src/platform/mod.rs
+++ d14/src/platform/mod.rs
@@ -1,6 +1,6 @@
-use core::fmt::Write;
+use core::fmt::{Display, Write};
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub(crate) enum MaybeRock {
     None,
     Rounded,
@@ -18,10 +18,22 @@ impl core::fmt::Debug for MaybeRock {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub(crate) struct Platform(pub(crate) Vec<Vec<MaybeRock>>);
 
 mod parse;
+
+impl Display for Platform {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for row in &self.0 {
+            for mr in row {
+                write!(f, "{mr:?}")?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
 
 impl Platform {
     fn height(&self) -> usize {
@@ -31,9 +43,14 @@ impl Platform {
         self.0[0].len()
     }
 
-    pub(crate) fn tilt_north(&mut self) {
+    fn rows_mut(&mut self) -> &mut Vec<Vec<MaybeRock>> {
+        &mut self.0
+    }
+
+    pub(crate) fn tilt_north(&mut self) -> bool {
+        let mut changed;
         loop {
-            let mut changed = false;
+            changed = false;
             for y in 1..self.height() {
                 for x in 0..self.width() {
                     if matches!(self.0[y][x], MaybeRock::Rounded)
@@ -49,8 +66,61 @@ impl Platform {
                 break;
             }
         }
+        changed
+    }
+    pub(crate) fn tilt_west(&mut self) -> bool {
+        let mut changed = false;
+        for row in self.rows_mut() {
+            for part in row.split_mut(|mr| matches!(mr, MaybeRock::Cube)) {
+                // HACK: this will become a hard error once I update to 1.82.0
+                // TODO: rm the `allow` and the trait
+                #[allow(unstable_name_collisions)]
+                if !part.is_sorted_by(|a, b| b <= a) {
+                    changed = true;
+                }
+                part.sort_unstable_by(|a, b| b.cmp(a));
+            }
+        }
+        changed
     }
 
+    pub(crate) fn tilt_south(&mut self) -> bool {
+        let mut changed;
+        loop {
+            changed = false;
+            for y in 0..self.height() - 1 {
+                for x in 0..self.width() {
+                    if matches!(self.0[y][x], MaybeRock::Rounded)
+                        && matches!(self.0[y + 1][x], MaybeRock::None)
+                    {
+                        let (above, below) = self.0.split_at_mut(y + 1);
+                        std::mem::swap(&mut above[y][x], &mut below[0][x]);
+                        changed = true;
+                    }
+                }
+            }
+            if !changed {
+                break;
+            }
+        }
+        changed
+    }
+
+    pub(crate) fn tilt_east(&mut self) -> bool {
+        let mut changed = false;
+        for row in self.rows_mut() {
+            for part in row.split_mut(|mr| matches!(mr, MaybeRock::Cube)) {
+                // HACK: this will become a hard error once I update to 1.82.0
+                // TODO: rm the `allow` and the trait
+                #[allow(unstable_name_collisions)]
+                if !part.is_sorted() {
+                    changed = true;
+                }
+                part.sort_unstable();
+            }
+        }
+        changed
+    }
     pub(crate) fn north_load(&self) -> usize {
         self.0
             .iter()
@@ -62,5 +132,34 @@ impl Platform {
                 })
             })
             .sum()
+    }
+}
+
+trait IsSorted
+where
+    Self::Item: Ord,
+{
+    type Item;
+
+    fn is_sorted(&self) -> bool {
+        self.is_sorted_by(|a, b| a <= b)
+    }
+
+    fn is_sorted_by<FN>(&self, f: FN) -> bool
+    where
+        FN: Fn(&Self::Item, &Self::Item) -> bool;
+}
+
+impl<T> IsSorted for [T]
+where
+    T: Ord,
+{
+    type Item = T;
+
+    fn is_sorted_by<FN>(&self, f: FN) -> bool
+    where
+        FN: Fn(&Self::Item, &Self::Item) -> bool,
+    {
+        self.windows(2).all(|w| f(&w[0], &w[1]))
     }
 }
