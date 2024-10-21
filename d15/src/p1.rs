424929234acad59eebaf1b5d6746117b13f2e131
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-21 14:56:47 +0200

    feat(d15): p1 done!

--- d15/src/p1.rs
+++ d15/src/p1.rs
@@ -1,5 +1,12 @@
+fn hash(s: &str) -> u32 {
+    s.chars()
+        .map(|c| c as u32)
+        .inspect(|n| eprintln!("{} => {n}", *n as u8 as char))
+        .fold(0, |hash, c| ((hash + c) * 17) % 256)
+}
+
 pub fn p1(file: &str) -> u32 {
-    todo!()
+    file.strip_suffix('\n').unwrap().split(',').map(hash).sum()
 }
 
 #[cfg(test)]
@@ -10,7 +17,7 @@ mod test {
     const REAL: &str = include_str!("../inputs/real.txt");
 
     #[test_case(EXAMPLE => 1320)]
-    #[test_case(REAL => ignore)]
+    #[test_case(REAL => 515_495)]
     fn test_p1(inp: &str) -> u32 {
         p1(inp)
     }
