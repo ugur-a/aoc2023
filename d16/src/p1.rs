f88ef8cb63bb4638434daf378ad0bf8e6cf5e239
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-22 11:01:16 +0200

    style: don't prepend test names with `test_`, use `super::` to avoid name conflicts

--- d16/src/p1.rs
+++ d16/src/p1.rs
@@ -4,14 +4,13 @@ pub fn p1(_file: &str) -> anyhow::Result<usize> {
 
 #[cfg(test)]
 mod test {
-    use super::*;
     use test_case::test_case;
     const EXAMPLE: &str = include_str!("../inputs/example.txt");
     const REAL: &str = include_str!("../inputs/real.txt");
 
     #[test_case(EXAMPLE => 46)]
     #[test_case(REAL => ignore)]
-    fn test_p1(inp: &str) -> usize {
-        p1(inp).unwrap()
+    fn p1(inp: &str) -> usize {
+        super::p1(inp).unwrap()
     }
 }
--- d16/src/p2.rs
+++ d16/src/p2.rs
@@ -4,14 +4,13 @@ pub fn p2(_file: &str) -> anyhow::Result<usize> {
 
 #[cfg(test)]
 mod test {
-    use super::*;
     use test_case::test_case;
     const EXAMPLE: &str = include_str!("../inputs/example.txt");
     const REAL: &str = include_str!("../inputs/real.txt");
 
     #[test_case(EXAMPLE => ignore)]
     #[test_case(REAL => ignore)]
-    fn test_p2(inp: &str) -> usize {
-        p2(inp).unwrap()
+    fn p2(inp: &str) -> usize {
+        super::p2(inp).unwrap()
     }
 }
